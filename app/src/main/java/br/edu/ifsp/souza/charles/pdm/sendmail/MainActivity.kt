ea6beb34f0f5c6be3d7140691f2e37b5de81596b
souzafcharles <souzach@outlook.com>
2022-10-15 20:28:05 -0300

    Update Class MainActivity

--- app/src/main/java/br/edu/ifsp/souza/charles/pdm/sendmail/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/souza/charles/pdm/sendmail/MainActivity.kt
@@ -2,10 +2,16 @@ package br.edu.ifsp.souza.charles.pdm.sendmail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.edu.ifsp.souza.charles.pdm.sendmail.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val amb: ActivityMainBinding by lazy{
+        ActivityMainBinding.inflate(layoutInflater);
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(amb.root)
+
     }
 }
