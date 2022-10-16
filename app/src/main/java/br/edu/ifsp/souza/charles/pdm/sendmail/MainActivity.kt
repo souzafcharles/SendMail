2e17fc72f625e4d39422f0055525d7fe08b1fd73
souzafcharles <souzach@outlook.com>
2022-10-15 23:13:38 -0300

    Update Classe MainActivity

--- app/src/main/java/br/edu/ifsp/souza/charles/pdm/sendmail/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/souza/charles/pdm/sendmail/MainActivity.kt
@@ -1,10 +1,15 @@
 package br.edu.ifsp.souza.charles.pdm.sendmail
 
+import android.content.Intent
+import android.content.Intent.*
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.edu.ifsp.souza.charles.pdm.sendmail.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
+
     private val amb: ActivityMainBinding by lazy{
         ActivityMainBinding.inflate(layoutInflater);
     }
@@ -13,5 +18,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
 
+        amb.cleanBt.setOnClickListener{
+
+            with(amb){
+                toEt.setText("")
+                ccEt.setText("")
+                bccEt.setText("")
+                subjectEt.setText("")
+                messageEt.setText("")
+            }
+        }
+
+        amb.sendBt.setOnClickListener{
+            val sendMailIntent = Intent(ACTION_SENDTO)
+
+            with(sendMailIntent){
+                putExtra(EXTRA_EMAIL, arrayOf(amb.toEt.toString()))
+                putExtra(EXTRA_CC, arrayOf(amb.ccEt.toString()))
+                putExtra(EXTRA_BCC, arrayOf(amb.bccEt.toString()))
+                putExtra(EXTRA_SUBJECT, arrayOf(amb.subjectEt.toString()))
+                putExtra(EXTRA_TEXT, arrayOf(amb.messageEt.toString()))
+
+                type = "message/rfc822"
+                data = Uri.parse("mailto:")
+            }
+
+            val chooserIntent = Intent(ACTION_CHOOSER)
+            chooserIntent.putExtra(EXTRA_INTENT, sendMailIntent)
+            startActivity(chooserIntent)
+        }
     }
 }
